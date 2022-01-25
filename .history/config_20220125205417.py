c1386c68a233def290c94372912ccaceac62d283
ajeytiwary <ajeyt@students.iiserpune.ac.in>
2022-01-25 22:02:51 +0100

    path_error

--- .history/config_20220125205417.py
+++ .history/config_20220125205417.py
@@ -21,7 +21,7 @@ PATH_BASED_ROUTING = True
 
 
 # path = '/run/user/12096878/gvfs/sftp:host=juniper/export/scratch1/ajt/CME_playground/data/cme_dat/'
-path = 'data/cme_dat/'
+path = '/home/plasmion/CWI_analysis/CME_comparison/'
 earth_cme_path = path + 'cme_dat1/'
 path_cme_soho = path + 'cme_dat2/'
 path_helcats = path + 'cme_dat3/'
